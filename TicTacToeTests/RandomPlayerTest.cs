f8d9c0854ef129282e5e773331a0210f7de320fa
Thomas <perso.milton@gmail.com>
2024-12-19 14:22:31 +0100

    Add first unit test

---
+++ TicTacToeTests/RandomPlayerTest.cs
@@ -0,0 +1,27 @@
+﻿using CSharpFunctionalExtensions;
+using FluentAssertions;
+using TicTacToe;
+using TicTacToe.Boards;
+
+namespace TicTacToeTests
+{
+    public class RandomPlayerTest
+    {
+        [Fact]
+        public void GetNextMove_Default_ReturnAValidPlayerMove()
+        {
+            // Assign
+            RandomPlayer aiPlayer = new('O');
+
+            // Act
+            Result<PlayerMove> playerMove = aiPlayer.GetNextMove();
+
+            // Assert
+            playerMove.IsSuccess.Should().BeTrue();
+
+            playerMove.Value.Row.Should().BeInRange(1, 3);
+
+            playerMove.Value.Column.Should().BeInRange(1, 3);
+        }
+    }
+}
